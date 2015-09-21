142701798894159de43541a54e21f8e77a36ce47
mario <laserna.mario@gmail.com>
2015-09-21 18:24:17 -0500

    se crean migraciones para tags y profile

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,13 +1,18 @@
 <?php
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class UserTableSeeder extends Seeder{
 	public function run()
 	{
 		\DB::table('users')->insert(array(
-			'name'		=>	'Mario',
+			'first_name'=>	'Mario',
+			'last_name'	=>	'Laserna',
 			'email'		=>	'laserna.mario@gmail.com',
-			'password'	=>	\Hash::make('123')
+			'active'	=>	true,
+			'password'	=>	\Hash::make('123'),
+			'created_at'=>	Carbon::now(),
+			'updated_at'=>	Carbon::now()
 		));	
 	}
 }
